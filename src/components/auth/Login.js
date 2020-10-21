bee89cbc97d5d046d90ce46376e1b921bdc17a32
Alex <alex_edisson@hotmail.com>
2020-10-21 11:58:43 -0500

    first steps

---
+++ src/components/auth/Login.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [user, updateUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = user;
+
+  const handleOnChange = (e) => {
+    updateUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="form-user">
+      <div className="container-form shadow-dark">
+        <h1>Log in</h1>
+        <form onSubmit={handleOnSubmit}>
+          <div className="input-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Your email"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="input-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Your password"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="input-form">
+            <input
+              className="btn btn-primary btn-block"
+              type="submit"
+              value="Log In"
+            />
+          </div>
+        </form>
+        <p>Or</p>
+        <Link to={"/NewAccount"} className="link-account">
+          Create An Account
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/auth/NewAccount.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const NewAccount = () => {
+  const [user, updateUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const { name, email, password, confirmPassword } = user;
+
+  const handleOnChange = (e) => {
+    updateUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="form-user">
+      <div className="container-form shadow-dark">
+        <h1>Create An Account</h1>
+        <form onSubmit={handleOnSubmit}>
+          <div className="input-form">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="First &#38; Last name"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="input-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Your email"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="input-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Your password"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="input-form">
+            <label htmlFor="password">Confirm Password</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={confirmPassword}
+              placeholder="Confirm password"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="input-form">
+            <input
+              className="btn btn-primary btn-block"
+              type="submit"
+              value="Create An Account"
+            />
+          </div>
+        </form>
+        <p>Or</p>
+        <Link to={"/"} className="link-account">
+          Log In
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NewAccount;
